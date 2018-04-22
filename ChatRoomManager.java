115801616a2dde75a8b0a9dc3f30abcdf97b87a8
Xueliu Wang <xueliuwang@xuelius-mbp.bc.edu>
2018-04-22 14:57:12 -0400

    aa

--- ChatRoomManager.java
+++ ChatRoomManager.java
@@ -1,13 +1,22 @@
 
-import java.rmi.*;
+import java.rmi.registry.*;
 
 public class ChatRoomManager {
 	
+	
 	String ServerIp;
+	Registry registry;
 	
 	public ChatRoomManager(String ServerIP ){
 		this.ServerIp = ServerIp;
-
+		try {
+	        registry = LocateRegistry.getRegistry(ServerIp);
+	         
+	     } catch (Exception e) {
+	         System.err.println("Client exception: " + e.toString());
+	         e.printStackTrace();
+	     }
 	}
+	
 
 }
