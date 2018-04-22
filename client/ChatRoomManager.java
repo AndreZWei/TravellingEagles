e3bfc987183398ce783d1562cae4692c9eba3a61
Xueliu Wang <xueliuwang@xuelius-mbp.bc.edu>
2018-04-22 16:09:58 -0400

    edit

--- client/ChatRoomManager.java
+++ client/ChatRoomManager.java
@@ -2,7 +2,6 @@ package client;
 
 import java.rmi.*;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
@@ -12,10 +11,17 @@ import server.ServerAPI;
 
 public class ChatRoomManager {
 	
-	String ServerIp;
+	ServerAPI ServerIp;
+
+	DatagramSocket socket;
+
+	private int PORT;
+	private String IPaddress;
 	
-	public ChatRoomManager(String ServerIP ){
+	public ChatRoomManager(ServerAPI ServerIP,String IPadrress,int PORT) throws Exception{
 		this.ServerIp = ServerIp;
+		
+		this.socket = new DatagramSocket();
 
 	}
 
