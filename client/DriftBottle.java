9e8380e4cbdc99fb080ceebe3772d655312cfddf
Affy Wu <wutf@bc.edu>
2018-04-22 15:42:22 -0400

    drift bottle

---
+++ client/DriftBottle.java
@@ -0,0 +1,23 @@
+package client;
+
+import java.util.Objects;
+
+public class DriftBottle {
+    private Gift gift;
+    private String msg;
+
+    public DriftBottle(Gift gift, String msg) {
+        this.gift = gift;
+        this.msg = Objects.toString(gift.getTime())+": "+msg;
+    }
+
+    public String getMessage(){
+        return msg;
+    }
+
+    public Gift getGift(){
+        return gift;
+    }
+
+
+}
