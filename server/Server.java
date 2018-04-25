afb9b7fc33f3e4a9cc94c52a83fbec52a39c47a4
Ben Deng <ben237829624@gmail.com>
2018-04-25 15:42:07 -0400

    fixed server constructor

--- server/Server.java
+++ server/Server.java
@@ -24,6 +24,7 @@ public class Server extends RemoteServer implements ServerAPI {
 
     public Server() {
         rooms = new ArrayList<>();
+        sockets = new HashMap<>();
     }
 
     @Override
