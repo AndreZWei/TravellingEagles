31a36ba74225747c201d103f764cd2500c5fed9b
Ben Deng <ben237829624@gmail.com>
2018-04-29 16:55:59 -0400

    receive thread

--- client/ClientRoomReceiverThread.java
+++ client/ClientRoomReceiverThread.java
@@ -1,5 +1,10 @@
 package client;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class ClientRoomReceiverThread implements Runnable {
 	
 	private ChatRoomManager crm;
@@ -12,7 +17,25 @@ public class ClientRoomReceiverThread implements Runnable {
 	
 	@Override
 	public void run() {
-		
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket(crm.getPort());
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		while (true) {
+			byte[] buf = new byte[1024];
+			DatagramPacket p = new DatagramPacket(buf, buf.length);
+
+			try {
+				socket.receive(p);
+				String message = new String(p.getData(), 0, p.getLength());
+				System.out.println("Message:"+message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	
